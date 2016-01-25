b2a1e96bfb0f74614e07b35b4d7001e855b4d633
Lauren Spatz <laurens914@gmail.com>
2016-01-25 12:46:33 -0800

    added the general layout for adding a marker to the map

--- js/map.js
+++ js/map.js
@@ -1,7 +1,16 @@
 function initMap(){
   var mapView = document.getElementById('map');
   var map = new google.maps.Map(mapView,{
-    center: {lat: 44.540, lng: -78.546 },
+    center: {lat: 47.609895, lng:-122.330259},
     zoom: 7
   });
 }
+var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+var ratLatLng = {lat:46.1776465, lng:-117.6790793};
+function addMarker(){
+  var marker = new google.maps.Marker({
+    position: ratLatLng,
+    map: map
+  });
+}
+addMarker();
