0047b0ebfd9b32bbc036edab16c82fd3bfd7f5de
Lacey Vu <laceylin2010@gmail.com>
2016-01-25 13:32:54 -0800

    adding the object constructorsand functionality to pull from the JSON file

--- js/map.js
+++ js/map.js
@@ -1,16 +1,21 @@
+var hikeLocations = [
+  ['Rattlesnake Ridge', 46.4173432, -121.0270189],
+  ['Pretzel Tree Trail', 47.4808, -122.0554],
+  ['Lower Coal Creek Trail', 47.5542, -122.166],
+];
+
 function initMap(){
   var mapView = document.getElementById('map');
   var map = new google.maps.Map(mapView,{
     center: {lat: 47.609895, lng:-122.330259},
     zoom: 7
   });
-}
-var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-var ratLatLng = {lat:46.1776465, lng:-117.6790793};
-function addMarker(){
-  var marker = new google.maps.Marker({
-    position: ratLatLng,
-    map: map
-  });
-}
-addMarker();
+
+  var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+  for(var i = 0; i < hikeLocations.length; i++){
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(hikeLocations[i][1],hikeLocations[i][2]),
+      map: map
+    });
+  }
+};
