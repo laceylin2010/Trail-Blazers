0655b7353391669a7eac403193db77c503153c1a
Lacey Vu <laceylin2010@gmail.com>
2016-01-27 09:43:12 -0800

    fixing the controller for it to work on regular load

--- js/moderateController.js
+++ js/moderateController.js
@@ -2,9 +2,10 @@
   var moderateController = {};
 
   moderateController.index = function(){
-    $('.moderate-trails').on('click', function(){
+    $('.moderate-trails').on(function(){
       console.log('hey I am here');
       $('.hiking-pic-bar').hide();
+      $('#trail-template').hide();
       $('#map').toggle();
       $('.moderate-pictures').show();
 
