021306b7f9a1b4dc32f07a759b8d3715e31c1ce4
Lacey Vu <laceylin2010@gmail.com>
2016-01-27 10:57:10 -0800

    adding the controllers for the moderate hikes

--- js/littleSiController.js
+++ js/littleSiController.js
@@ -2,9 +2,13 @@
 
   var littleSiController = {};
 
-
-
-
+  littleSiController.index = function(){
+    $('hiking-pic-bar').hide();
+    $('.individual-trails').hide();
+    $('#trail-template').show();
+    $('#little-si').show();
+    $('#map').toggle();
+  };
 
   module.littleSiController = littleSiController;
 })(window);
